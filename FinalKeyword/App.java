eef3eee80844966eb30c435eaba4e095b0041e06
Swapnil-Tech-J <123949138+Swapnil-Tech-J@users.noreply.github.com>
2023-01-31 13:57:10 +0530

    Add files via upload

---
+++ FinalKeyword/App.java
@@ -0,0 +1,12 @@
+package FinalKeyword;
+
+public class App {
+
+	public static void main(String[] args) {
+	
+		B obj1 = new B(12);
+		System.out.println(obj1.getX());
+		
+	}
+ 
+}
---
+++ FinalKeyword/B.java
@@ -0,0 +1,22 @@
+package FinalKeyword;
+
+public class B extends A {
+      private final int x;
+
+
+
+	public B(int x) {
+		super();
+		this.x = x;
+		
+	}
+
+	public int getX() {
+		return x;
+	}
+
+
+	
+      
+      
+}
