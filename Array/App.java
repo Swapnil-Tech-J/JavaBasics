894b18b9ed3267234d346ebc30c8d8184cc82289
Swapnil-Tech-J <123949138+Swapnil-Tech-J@users.noreply.github.com>
2023-01-31 14:00:46 +0530

    Create App.java

---
+++ Array/App.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class App {
+
+	public static void main(String[] args) {
+		int [] intVariableArray= {1,2,3,4,5,6,7,8,9,10};
+		System.out.println(intVariableArray[2]);
+		System.out.println(intVariableArray.length);
+		
+		String [] Names = {"one","two","three","four","five"};
+		System.out.println(Names[2]);
+	
+		
+	
+
+	}
+
+}
