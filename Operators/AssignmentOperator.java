4fc86ecefb3376c2bf42187c529628bc50fc8d59
Swapnil-Tech-J <123949138+Swapnil-Tech-J@users.noreply.github.com>
2023-01-31 14:10:30 +0530

    Update AssignmentOperator.java

--- Operators/AssignmentOperator.java
+++ Operators/AssignmentOperator.java
@@ -1,1 +1,20 @@
+package Operators;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		int x=5;
+		//x+=5; //x = x + 5
+	    //x-=5; //x = x - 5
+		//x*=5; //x = x * 5
+		//x/=5; //x = x / 5
+		//x%=4; //x = x / 4
+		System.out.println(x);
+		
+		
+		
+
+	} 
+
+}
 
