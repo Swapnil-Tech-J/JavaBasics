59df0c0c12e385b45166f34a937358e04eefb646
Swapnil-Tech-J <123949138+Swapnil-Tech-J@users.noreply.github.com>
2023-02-23 09:51:37 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,32 @@
+package Interview;
+
+import java.util.Scanner;
+
+public class Palindrome {
+	
+	 private static boolean isPalindrome(int x) {
+	        // Base condition
+	        if (x < 0) {
+	            return false;
+	        }
+	        // Store the number in a variable
+	        int number = x;
+	        // This will store the reverse of the number
+	        int reverse = 0;
+	        while (number > 0) {
+	            reverse = reverse * 10 + number % 10;
+	            number /= 10;
+	        }
+	        return x == reverse;
+	    }
+
+	    public static void main(String[] args) {
+	        System.out.println(isPalindrome(121));
+	        System.out.println(isPalindrome(-121));
+	        System.out.println(isPalindrome(10));
+	        System.out.println(isPalindrome(-101));
+	    }
+	}
+
+
+	
