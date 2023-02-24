aeaf3fa89e1f75d695d1101cb318ad19c9476cea
Swapnil-Tech-J <123949138+Swapnil-Tech-J@users.noreply.github.com>
2023-02-24 14:19:41 +0530

    Add files via upload
    This code file elaborates the use of flag variable in Java.

---
+++ Pn.java
@@ -0,0 +1,35 @@
+package Interview;
+
+import java.util.Scanner;
+
+public class Pn {
+	public static void main(String[] args) {
+		
+		boolean flag = false;
+		final double PASS_MARK=80;
+		double userScore =95;
+		
+		if(userScore>=PASS_MARK) {
+			flag=true;
+			
+		}
+		
+		if(flag) {
+			System.out.println("You passed. Congrats!");
+		}
+		else if(flag!=true) {
+			System.out.println("fail");
+		}
+		
+	
+		}
+		
+		
+		
+		
+	}
+	
+	
+
+		
+	
